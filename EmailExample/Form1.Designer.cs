424757a29f036d4f1c05e404803faea6baefb293
p3t3r276 <p3t3r276@gmail.com>
2019-07-24 02:26:54 +0700

    Hoàn thành

--- EmailExample/Form1.Designer.cs
+++ EmailExample/Form1.Designer.cs
@@ -35,23 +35,25 @@
             this.txtSubject = new System.Windows.Forms.TextBox();
             this.txtContent = new System.Windows.Forms.TextBox();
             this.btnGui = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtReceptName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblRecipe
             // 
             this.lblRecipe.AutoSize = true;
             this.lblRecipe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRecipe.Location = new System.Drawing.Point(13, 26);
+            this.lblRecipe.Location = new System.Drawing.Point(13, 24);
             this.lblRecipe.Name = "lblRecipe";
-            this.lblRecipe.Size = new System.Drawing.Size(123, 20);
+            this.lblRecipe.Size = new System.Drawing.Size(92, 20);
             this.lblRecipe.TabIndex = 0;
-            this.lblRecipe.Text = "Tên người nhận:";
+            this.lblRecipe.Text = "Email nhận:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 57);
+            this.label1.Location = new System.Drawing.Point(13, 93);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 20);
             this.label1.TabIndex = 0;
@@ -61,7 +63,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 92);
+            this.label2.Location = new System.Drawing.Point(13, 128);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(103, 20);
             this.label2.TabIndex = 0;
@@ -70,7 +72,7 @@
             // txtReceiver
             // 
             this.txtReceiver.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtReceiver.Location = new System.Drawing.Point(216, 23);
+            this.txtReceiver.Location = new System.Drawing.Point(158, 21);
             this.txtReceiver.Name = "txtReceiver";
             this.txtReceiver.Size = new System.Drawing.Size(400, 26);
             this.txtReceiver.TabIndex = 0;
@@ -78,40 +80,61 @@
             // txtSubject
             // 
             this.txtSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSubject.Location = new System.Drawing.Point(216, 54);
+            this.txtSubject.Location = new System.Drawing.Point(158, 90);
             this.txtSubject.Name = "txtSubject";
             this.txtSubject.Size = new System.Drawing.Size(400, 26);
-            this.txtSubject.TabIndex = 1;
+            this.txtSubject.TabIndex = 2;
             // 
             // txtContent
             // 
             this.txtContent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtContent.Location = new System.Drawing.Point(216, 92);
+            this.txtContent.Location = new System.Drawing.Point(158, 128);
             this.txtContent.Multiline = true;
             this.txtContent.Name = "txtContent";
             this.txtContent.Size = new System.Drawing.Size(400, 248);
-            this.txtContent.TabIndex = 2;
+            this.txtContent.TabIndex = 3;
             // 
             // btnGui
             // 
             this.btnGui.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGui.Location = new System.Drawing.Point(517, 356);
+            this.btnGui.Location = new System.Drawing.Point(483, 396);
             this.btnGui.Name = "btnGui";
             this.btnGui.Size = new System.Drawing.Size(75, 32);
-            this.btnGui.TabIndex = 3;
+            this.btnGui.TabIndex = 4;
             this.btnGui.Text = "Gửi";
             this.btnGui.UseVisualStyleBackColor = true;
+            this.btnGui.Click += new System.EventHandler(this.btnGui_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tên người nhận:";
+            // 
+            // txtReceptName
+            // 
+            this.txtReceptName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReceptName.Location = new System.Drawing.Point(158, 53);
+            this.txtReceptName.Name = "txtReceptName";
+            this.txtReceptName.Size = new System.Drawing.Size(400, 26);
+            this.txtReceptName.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(655, 391);
+            this.ClientSize = new System.Drawing.Size(587, 440);
             this.Controls.Add(this.btnGui);
             this.Controls.Add(this.txtContent);
             this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtReceptName);
             this.Controls.Add(this.txtReceiver);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblRecipe);
             this.Name = "Form1";
@@ -131,6 +154,8 @@
         private System.Windows.Forms.TextBox txtSubject;
         private System.Windows.Forms.TextBox txtContent;
         private System.Windows.Forms.Button btnGui;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtReceptName;
     }
 }
 
--- EmailExample/Form1.cs
+++ EmailExample/Form1.cs
@@ -1,25 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 using System.Windows.Forms;
 
 namespace EmailExample
 {
     public partial class Form1 : Form
     {
+        private SmtpClient smtpClient = null;
+        private MailAddress fromAddress = null; 
+        const string fromPassword = "password"; // TODO: Thêm pass của mính
+
         public Form1()
         {
             InitializeComponent();
-            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            fromAddress = new MailAddress("email gửi", "Tên người gửi"); // TODO: Thêm email của mình
+            smtpClient = new SmtpClient
+            {
+                Host = "smtp.gmail.com", // Mặc định của gmail
+                Port = 587, // Mặc định của gmail
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false, // Nếu để true sẽ gửi thông tin đăng nhập của máy tính, ở đây mình dùng thông tin dăng nhập gmail
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+        }
+
+        private void btnGui_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var toAddress = new MailAddress(txtReceiver.Text, txtReceptName.Text != String.Empty ? txtReceptName.Text : "Tên mặc định");
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = txtSubject.Text,
+                    Body = txtContent.Text,
+                    IsBodyHtml = true // Cho phép gửi HTML
+                })
+                {
+                    smtpClient.Send(message);
+                }
+                MessageBox.Show("Thư của bạn đã gửi thành công đến " + toAddress.Address, "Gửi thư thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Xảy ra lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }     
         }
     }
 }
